f1ac1159bb02e51b9d5b4c2cdccdc9953e518627
PhuocThiem <duongphuocthiem@gmail.com>
2022-11-25 22:53:39 +0700

    fix layout of search bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,9 @@
-<template>
-  <div>
-    <PageTitle> Dashboard </PageTitle>
-    <div class="flex flex-col p-3 w-full h-full">
-      <div class="flex flex-row justify-start w-full items-center h-20 gap-2">
-        <div class="w-1/3 shadow-sm">
-          <label for="price" class="block text-sm font-medium text-gray-700"
-            >Search</label
-          >
-          <input
-            v-model="searchText"
-            type="text"
-            placeholder="Input task title"
-            class="w-full border-solid border-b-[2px] border-neutral-400 focus:border-b-[2px] focus:border-sky-600 focus:outline-none"
-          />
-        </div>
-        <div class="w-1/4 shadow-sm border-b-sky-900">
-          <label for="price" class="block text-sm font-medium text-gray-700"
-            >Sort by title</label
-          >
-          <select
-            v-model="selectedSortTitle"
-            class="w-full border-solid border-b-[2px] border-neutral-400 focus:border-b-[2px] focus:border-sky-600 focus:outline-none"
-          >
-            <option v-for="type in SortType" :key="type.id" :value="type.id">
-              {{ type.type }}
-            </option>
-          </select>
-        </div>
-        <div class="w-1/4 shadow-sm border-b-sky-900">
-          <label for="price" class="block text-sm font-medium text-gray-700"
-            >Sort by expired date</label
-          >
-          <select
-            v-model="selectedSortExpired"
-            class="w-full border-solid border-b-[2px] border-neutral-400 focus:border-b-[2px] focus:border-sky-600 focus:outline-none"
-          >
-            <option v-for="type in SortType" :key="type.id" :value="type.id">
-              {{ type.type }}
-            </option>
-          </select>
-        </div>
-        <button
-          class="w-[100px] hover:bg-slate-200 flex flex-row justify-center ml-20 mt-[22px]"
-        >
-          <Icon :icon-path="ICON_PATH.ADDITION" />
-        </button>
-      </div>
-      <Table :tickets-list="tableData" />
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
-import { ENDPOINT } from "~~/constants/endpoint";
-import { useTicketStore } from "~~/store/ticket";
-import { Ticket } from "~~/store/models/ticket";
-import { PageTitle, Table, Icon, ICON_PATH } from "~~/components";
-import { SortType } from "~~/constants/sortType";
+import { ENDPOINT } from '~~/constants/endpoint';
+import { useTicketStore } from '~~/store/ticket';
+import { Ticket } from '~~/store/models/ticket';
+import { PageTitle, Table, Icon, ICON_PATH } from '~~/components';
+import { SortType } from '~~/constants/sortType';
 
 const ticket = useTicketStore();
 
@@ -64,13 +11,13 @@ const config = useRuntimeConfig();
 const { baseURL } = config.public;
 
 const tableData = ref<Ticket[]>([]);
-const searchText = ref<String>("");
+const searchText = ref<String>('');
 const timeoutID = ref<any>(null);
 const selectedSortTitle = ref<Number>(SortType[0]?.id);
 const selectedSortExpired = ref<Number>(SortType[0]?.id);
 
 const { data, error } = await useFetch(ENDPOINT.TICKETS, {
-  method: "GET",
+  method: 'GET',
   baseURL,
   onResponse({ response }) {
     ticket.updateGetTicketsState(response._data);
@@ -101,9 +48,7 @@ watch(selectedSortExpired, () => {
 });
 
 function searchByText(text: string) {
-  tableData.value = (data.value as Array<Ticket>).filter((ticket: Ticket) =>
-    ticket?.title.includes(text)
-  );
+  tableData.value = (data.value as Array<Ticket>).filter((ticket: Ticket) => ticket?.title.includes(text));
 }
 
 function sortByTitle() {
@@ -120,7 +65,7 @@ function sortByTitle() {
 }
 
 function sortByExpiredDate() {
-  console.log("sort by Expire date", selectedSortExpired.value);
+  console.log('sort by Expire date', selectedSortExpired.value);
   switch (selectedSortExpired.value) {
     case SortType[1]?.id:
       tableData.value = _sortExpiredAsc([...(data.value as Array<Ticket>)]);
@@ -147,17 +92,60 @@ function _sortArrDsc(arr: Ticket[]) {
 
 function _sortExpiredAsc(arr: Ticket[]) {
   return arr.sort((a: Ticket, b: Ticket) =>
-    new Date(a.ExpiredDate).getTime() > new Date(b.ExpiredDate).getTime()
-      ? 1
-      : -1
+    new Date(a.ExpiredDate).getTime() > new Date(b.ExpiredDate).getTime() ? 1 : -1
   );
 }
 
 function _sortExpiredDsc(arr: Ticket[]) {
   return arr.sort((a: Ticket, b: Ticket) =>
-    new Date(a.ExpiredDate).getTime() > new Date(b.ExpiredDate).getTime()
-      ? -1
-      : 1
+    new Date(a.ExpiredDate).getTime() > new Date(b.ExpiredDate).getTime() ? -1 : 1
   );
 }
 </script>
+
+<template>
+  <div>
+    <PageTitle> Dashboard </PageTitle>
+    <div class="flex flex-col p-3 w-full h-full">
+      <div class="flex flex-row justify-start w-full items-center h-20 gap-2">
+        <div class="w-1/3 shadow-sm">
+          <label for="price" class="block text-sm font-medium text-gray-700">Search</label>
+          <input
+            v-model="searchText"
+            type="text"
+            placeholder="Input task title"
+            class="w-full border-solid border-b-[2px] border-neutral-400 focus:border-b-[2px] focus:border-sky-600 focus:outline-none h-[40px]"
+          />
+        </div>
+        <div class="w-1/4 shadow-sm border-b-sky-900">
+          <label for="price" class="block text-sm font-medium text-gray-700">Sort by title</label>
+          <select
+            v-model="selectedSortTitle"
+            class="w-full border-solid border-b-[2px] border-neutral-400 focus:border-b-[2px] focus:border-sky-600 focus:outline-none h-[40px]"
+          >
+            <option v-for="type in SortType" :key="type.id" :value="type.id">
+              {{ type.type }}
+            </option>
+          </select>
+        </div>
+        <div class="w-1/4 shadow-sm border-b-sky-900">
+          <label for="price" class="block text-sm font-medium text-gray-700">Sort by expired date</label>
+          <select
+            v-model="selectedSortExpired"
+            class="w-full border-solid border-b-[2px] border-neutral-400 focus:border-b-[2px] focus:border-sky-600 focus:outline-none h-[40px]"
+          >
+            <option v-for="type in SortType" :key="type.id" :value="type.id">
+              {{ type.type }}
+            </option>
+          </select>
+        </div>
+        <div class="h-[60px] flex flex-row justify-center items-end">
+          <button class="w-[100px] hover:bg-slate-200 ml-20 items-center h-[40px] flex justify-center">
+            <Icon :icon-path="ICON_PATH.ADDITION" />
+          </button>
+        </div>
+      </div>
+      <Table :tickets-list="tableData" />
+    </div>
+  </div>
+</template>
