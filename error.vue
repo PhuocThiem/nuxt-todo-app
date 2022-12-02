ef0dbf2015dbf9e2471da99db3d352b0c5ffb326
Thiem Duong <duongphuocthiem@gmail.com>
2022-12-02 11:25:37 +0700

    config apply tailwind for error.vue root directory

--- error.vue
+++ error.vue
@@ -1,33 +1,22 @@
-<template>
-  <div class="w-screen h-screen flex flex-col justify-center items-center bg-gradient-to-r from-teal-600 bg-cyan-500">
-    <h1 class="text-9xl font-bold text-white">{{ error?.statusCode }}</h1>
-    <p class="text-white message">{{ error?.statusMessage }}</p>
-    <NuxtLink class="link" to="/">Back to Home</NuxtLink>
-  </div>
-</template>
-
 <script setup lang="ts">
+useHead({
+  titleTemplate: 'Error',
+  htmlAttrs: {
+    lang: 'en',
+  },
+});
+
 defineProps({
   error: Object,
 });
 </script>
 
-<style scoped lang="scss">
-.title {
-  // font-size: 160px;
-}
-.message {
-  font-size: 4rem;
-}
-.link {
-  background-color: rgb(59 130 246);
-  padding: 10px 20px;
-  color: white;
-  border-radius: 5px;
-  font-size: 20px;
-  &:hover {
-    background-color: rgb(30 58 138);
-  }
-  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-}
-</style>
+<template>
+  <div class="w-screen h-screen flex flex-col justify-center items-center bg-gradient-to-r from-teal-600 bg-cyan-500">
+    <h1 class="text-9xl font-bold text-white">{{ error?.statusCode }}</h1>
+    <p class="text-6xl text-white">{{ error?.statusMessage }}</p>
+    <NuxtLink class="bg-blue-500 px-5 py-1 text-white rounded text-2xl hover:bg-blue-900 shadow-xl mt-2" to="/"
+      >Back to Home</NuxtLink
+    >
+  </div>
+</template>
--- pages/tasks/[id].vue
+++ pages/tasks/[id].vue
@@ -1,22 +1,3 @@
-<template>
-  <div>
-    <PageTitle> Task Information </PageTitle>
-    <div class="flex flex-col items-center w-full py-10">
-      <div class="shadow-lg shadow-gray-400 bg-white w-1/2 p-10">
-        <ErrorMessage v-if="error" :err-msg="`Cannot get info of task id ${route.params?.id}`" />
-        <div v-else>
-          <p class="text-xl">Title: {{ data?.title }}</p>
-          <p class="text-xl">Note: {{ data?.note }}</p>
-          <p class="text-xl flex">Status: <Tag :is-completed="data?.isCompleted" /></p>
-          <p class="text-xl">created at: {{ moment(data?.createdAt).format('LL') }}</p>
-          <p class="text-xl">Assigned to: {{ data?.assignTo }}</p>
-          <p class="text-xl">Expired date: {{ moment(data?.ExpiredDate).format('LL') }}</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { PageTitle, Tag, ErrorMessage } from '~~/components';
 import { ENDPOINT } from '~~/constants/endpoint';
@@ -36,3 +17,22 @@ const { baseURL } = config.public;
 
 const { data, error } = await useFetch(`${baseURL}${ENDPOINT.TICKETS}/${route.params?.id}`);
 </script>
+
+<template>
+  <div>
+    <PageTitle> Task Information </PageTitle>
+    <div class="flex flex-col items-center w-full py-10">
+      <div class="shadow-lg shadow-gray-400 bg-white w-1/2 p-10">
+        <ErrorMessage v-if="error" :err-msg="`Cannot get info of task id ${route.params?.id}`" />
+        <div v-else>
+          <p class="text-xl">Title: {{ data?.title }}</p>
+          <p class="text-xl">Note: {{ data?.note }}</p>
+          <p class="text-xl flex">Status: <Tag :is-completed="data?.isCompleted" /></p>
+          <p class="text-xl">created at: {{ moment(data?.createdAt).format('LL') }}</p>
+          <p class="text-xl">Assigned to: {{ data?.assignTo }}</p>
+          <p class="text-xl">Expired date: {{ moment(data?.ExpiredDate).format('LL') }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
